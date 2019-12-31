60e11514bc5a1bf20531bb517fc8149d0cd979ec
Ayako Wiltshire <awittyl44@gmail.com>
2019-12-31 15:15:08 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -14,8 +14,8 @@ class Waiter
     @@all
   end
 
-  def new_meal(customer, total, tip= 0)
-    Meal.new(customer, self, total, tip)
+  def new_meal(name, years_of_exp)
+    Meal.new(self, name, years_of_exp)
   end
 
 
