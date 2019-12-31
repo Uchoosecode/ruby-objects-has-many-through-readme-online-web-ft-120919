126227d9420d30f413b94a4bf8db5cff2c648398
Ayako Wiltshire <awittyl44@gmail.com>
2019-12-31 14:33:50 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,19 @@
 class Waiter
 
+  attr_accessor :name, :years_of_exp
+  
+  @@all = []
+  
+  def initialize(name, years_of_exp)
+    @name = name
+    @years_of_exp = years_of_exp
+    @@all << self
+  end
+
+
+
+
+
+
+
 end
